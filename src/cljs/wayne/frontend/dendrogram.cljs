76a19324850ace9ceebd74d916a99b23b8ae69fa
Mike Travers <mt@hyperphor.com>
2024-03-27 19:36:52 -0700

    composite view

--- src/cljs/wayne/frontend/dendrogram.cljs
+++ src/cljs/wayne/frontend/dendrogram.cljs
@@ -72,42 +72,26 @@
 
 (def spec
   {:description "An example of Cartesian layouts for a node-link diagram of hierarchical data.",
-   :width 600,
+   :width 400,
+   :$schema "https://vega.github.io/schema/vega/v5.json",
+   :height 400,
+
+   ;; Therese are trying to get layout right
+   ;; :autosize "pad"
+   :layout {                            ;yes having an empty element here makes a differnece
+            ;;:padding 70,     
+            ;; :bounds "flush",
+            ;; :align "none"
+            },
+   ;; :config {:axisY {:minExtent 30}}
+   ;; END
+
    :scales
    [{:name "color",
      :type "linear",
      :range {:scheme "magma"},
      :domain {:data "tree", :field "depth"},
      :zero true}],
-   :padding 5,
-   :marks
-   [{:type "path",
-     :from {:data "links"},
-     :encode {:update {:path {:field "path"}, :stroke {:value "#ccc"}}}}
-    #_{:type "symbol",
-       :from {:data "tree"},
-       :encode
-       {:enter {:size {:value 100}, :stroke {:value "#fff"}},
-        :update {:x {:field "x"}, :y {:field "y"}, :fill {:scale "color", :field "depth"}}}}
-    {:type "text",
-     :from {:data "tree"},
-     :encode
-     {:enter {:text {:field "name"}, :fontSize {:value 9}, :baseline {:value "middle"}},
-      :update
-      {:x {:field "x"},
-       :y {:field "y"},
-       :dx {:signal "datum.children ? -7 : 7"},
-       :align {:signal "datum.children ? 'right' : 'left'"},
-       :opacity {:signal "labels ? 1 : 0"}}}}],
-   :$schema "https://vega.github.io/schema/vega/v5.json",
-   :signals
-   [{:name "labels", :value true, :bind {:input "checkbox"}}
-    #_ {:name "layout", :value "tidy", :bind {:input "radio", :options ["tidy" "cluster"]}}
-    #_ {:name "links",
-        :value "diagonal",
-        :bind {:input "select", :options ["line" "curve" "diagonal" "orthogonal"]}}
-    {:name "separation", :value false, :bind {:input "checkbox"}}],
-   :height 1600,
    :data
    [{:name "tree",
      :url "https://vega.github.io/vega/data/flare.json",
@@ -122,7 +106,68 @@
      :source "tree",
      :transform
      [{:type "treelinks"}
-      {:type "linkpath", :orient "horizontal", :shape "orthogonal"}]}]})
+      {:type "linkpath", :orient "horizontal", :shape "orthogonal"}]}]
+   :signals
+   [{:name "labels", :value true, :bind {:input "checkbox"}}
+    #_ {:name "layout", :value "tidy", :bind {:input "radio", :options ["tidy" "cluster"]}}
+    #_ {:name "links",
+        :value "diagonal",
+        :bind {:input "select", :options ["line" "curve" "diagonal" "orthogonal"]}}
+    {:name "separation", :value false, :bind {:input "checkbox"}}]
+   :padding 5,
+   :marks
+   [{:type "group"
+     :style "cell"
+     :title {:text "nothingness" :frame "group"}
+     :encode {:update {:width {:signal "height"}, ;This is what finally got the layout semi-sane
+                       :height {:signal "height"}
+        }
+      }
+     :marks [{:type "path",
+              :from {:data "links"},
+              :encode {:update {:path {:field "path"}, :stroke {:value "#ccc"}}}}
+             #_{:type "symbol",
+                :from {:data "tree"},
+                :encode
+                {:enter {:size {:value 100}, :stroke {:value "#fff"}},
+                 :update {:x {:field "x"}, :y {:field "y"}, :fill {:scale "color", :field "depth"}}}}
+             {:type "text",
+              :from {:data "tree"},
+              :encode
+              {:enter {:text {:field "name"}, :fontSize {:value 9}, :baseline {:value "middle"}},
+               :update
+               {:x {:field "x"},
+                :y {:field "y"},
+                :dx {:signal "datum.children ? -7 : 7"},
+                :align {:signal "datum.children ? 'right' : 'left'"},
+                :opacity {:signal "labels ? 1 : 0"}}}}],
+     }
+
+    ;; Second view
+    {:type "group"
+     :name "heatmap"
+     :style "cell"
+     :title {:text "your ad here" :frame "group"}
+;     :width 400
+;     :height 400
+     :encode {
+              :update {
+                       :width {:signal "height"},
+                       :height {:signal "height"}
+        }
+      },
+
+    :marks
+     [{:type "rect"
+       :from  {:data "tree"}
+        :encode
+        {:enter
+           {:y {:field "x"}
+            :x {:field "y"}
+            :width {:value 15}
+            :height {:value 5}}}}
+      ]
+     }]}) 
 
 (defn dendrogram
   []  
@@ -130,5 +175,4 @@
 
 (defn ui
   []
-  [:div "Hey loser"
-   [dendrogram]])
+  [dendrogram])
