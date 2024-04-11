2cc805899653a6c4799d4ad233e24002324feb70
Mike Travers <mt@hyperphor.com>
2024-04-10 20:01:26 -0700

    Componentize fgrid and wire into universal query (partly)

--- src/cljs/wayne/frontend/fgrid.cljs
+++ src/cljs/wayne/frontend/fgrid.cljs
@@ -1,19 +1,28 @@
 (ns wayne.frontend.fgrid
   (:require [way.vega :as v]
+            [org.candelbio.multitool.core :as u]
             )
   )
 
-(def spec
+(defn spec
+  [dim]
   {:data {:url "filter-grid.js"}
+   :transform (if dim
+                [{:filter (u/expand-template "test(/^{dim}/, datum.dim1) && !(test(/^{dim}/, datum.dim2))"
+                                             {"dim" (name dim)})}]
+                [])
    :mark {:type "rect", :tooltip {:content "data"} },
    :encoding
-   {:x {:field "dim1" :type "nominal" :axis {:grid true :bandPosition 0}}
-    :y {:field "dim2" :type "nominal" :axis {:grid true :bandPosition 0}}
+   {:x {:field "dim2" :type "nominal" :axis {:grid true :bandPosition 0 :orient :top} :title false}
+    :y {:field "dim1" :type "nominal" :axis {:grid true :bandPosition 0} :title false}
     :color {:field "count" :type "quantitative" :scale {:domain [0 100]}}
+    ;; TODO damn it this does not work, apparently adding event handlers is rocket science to vega lite. 
+    ;; In reality, this should populate the filter appropriately. 
+    :href {:value "javascript:print(\"foo\")"} ; 
     }
    })
 
 (defn ui
-  []
+  [& [dim]]
   [:div.p-5
-   [v/vega-lite-view spec []]])
+   [v/vega-lite-view (spec dim) []]])
