9642aa74ce0a1b2e4888aa499693bc47f9dafd58
Mike Travers <mt@hyperphor.com>
2024-06-12 15:21:10 -0700

    update munson for new way init mech

--- src/cljs/wayne/frontend/munson.cljs
+++ src/cljs/wayne/frontend/munson.cljs
@@ -4,11 +4,9 @@
    [goog.dom :as gdom]
    [reagent.core :as r]
    [re-frame.core :as rf]
+   [hyperphor.way.ui.init :as init]
    [wayne.frontend.universal :as universal]
    [wayne.frontend.feature-select :as fui]
-
-   #_ [wayne.frontend.signup :as signup]
-
    [org.candelbio.multitool.core :as u]
    ))
 
@@ -132,20 +130,7 @@
    #_ [universal/ui]
    ])
 
-;;; Note: the dev/after-load interferes with regular app
-(defn mount-root ;  ^:dev/after-load
-  []
-  ;; The `:dev/after-load` metadata causes this function to be called
-  ;; after shadow-cljs hot-reloads code.
-  ;; This function is called implicitly by its annotation.
-  (rf/clear-subscription-cache!)
-  (let [root (createRoot (gdom/getElement "app"))]
-    (.render root (r/as-element [app-ui]))
-    )
-  )
-
 (defn ^:export init
   []
-  (rf/dispatch-sync [:initialize-db])
-  (mount-root)
+  (init/init app-ui nil)
   )
