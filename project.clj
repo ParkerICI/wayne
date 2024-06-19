7f2fa7eda5d94f5fee7a308042d784b4340d6f4f
Mike Travers <mt@hyperphor.com>
2024-06-18 21:00:55 -0700

    trim dependencies; make simple tabs use new feed data scheme

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
-;;; Based on Catamite and Traverse. Trying to keep this usable as a template
-
 (defproject wayne "0.1.0-SNAPSHOT"
   :description "Prototyping BRUCE website"
   :min-lein-version "2.0.0"
@@ -9,41 +7,19 @@
                  [org.clojure/data.json "2.5.0"]
                  [environ "1.2.0"]
                  [com.taoensso/timbre "6.3.1"]
-                 [hyperphor/way "0.1.0-SNAPSHOT"] ;TODO npm dependencies
-
-                 ;; Backend
-                 [clj-http "3.12.3" :exclusions [commons-io]]
-                 [compojure "1.7.0"]
-                 [ring "1.11.0"]
-                 [ring/ring-core "1.11.0"]
-                 [ring/ring-defaults "0.4.0"]
-                 [ring/ring-jetty-adapter "1.11.0"]
-                 [ring-basic-authentication "1.2.0"]
-                 [ring-logger "1.1.1"]
-                 [ring-middleware-format "0.7.5" :exclusions [javax.xml.bind/jaxb-api]]
-                 ;; Data
-                 [clj-http "3.12.3" :exclusions [commons-io]]
-                 [alandipert/enduro "1.2.0"] ;persistence for expensive calculations
-                 ;; frontend
-                 ;; See packge.json for the real ones
-                 #_ [org.clojure/clojurescript "1.11.132"] ;causes shadow-cljs error, who knows
-                 [thheller/shadow-cljs "2.26.5"] ;TODO maybe only in dev profile
-                 [reagent "1.2.0"]
-                 [re-frame "1.4.2"]
-                 [com.cemerick/url "0.1.1"]
-                 [cljs-ajax "0.8.0"]
-                 [day8.re-frame/tracing "0.6.2"]      ;TODO dev only
-                 [day8.re-frame/re-frame-10x "1.9.3"] ;TODO dev only
-
+                 [hyperphor/way "0.1.1-SNAPSHOT"] ;TODO npm dependencies
                  ;;; Dev only (TODO use profiles)
                  [seesaw "1.5.0"]
+
+                 ;; frontend
+                 ;; See packge.json for the real ones
+                 
+                 ;; This should get inherited from way, but is necessary for some reason
+                 [thheller/shadow-cljs "2.26.5"] ;TODO maybe only in dev profile
 
                  ;;; Wayne specific
 
                  [com.google.cloud/google-cloud-bigquery "2.27.0"]
-                 [com.google.cloud/google-cloud-datastore "2.14.7"]
-                 [com.google.cloud/google-cloud-storage "2.22.3"]
-
                  ]
   :main ^:skip-aot wayne.core
   :target-path "target/%s"
--- src/cljs/wayne/frontend/samples.cljs
+++ src/cljs/wayne/frontend/samples.cljs
@@ -1,7 +1,6 @@
 (ns wayne.frontend.samples
   (:require [re-frame.core :as rf]
             [hyperphor.way.aggrid :as ag]
-            [hyperphor.way.tabs :as tab]
             [reagent.dom]
             [wayne.frontend.data :as data]
             )
@@ -11,12 +10,9 @@
   []
   [:div
    [:h3 "Samples"]
-   (let [samples @(rf/subscribe [:data :samples])]
+   (let [samples @(rf/subscribe [:data [:samples]])]
      [ag/ag-table 
       samples
       :col-defs data/col-defs
       ])])
 
-(defmethod tab/set-tab [:tab :samples]
-  [id tab db]
-  (rf/dispatch [:fetch-once :samples]))
