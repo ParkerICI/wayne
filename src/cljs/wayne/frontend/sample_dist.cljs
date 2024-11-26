6b2e72695c31fb3f08399ba7b80e6b41f7d77bdc
Mike Travers <mt@hyperphor.com>
2024-11-25 19:21:04 -0800

    get rid of _ in sample matrix

--- src/cljs/wayne/frontend/sample_dist.cljs
+++ src/cljs/wayne/frontend/sample_dist.cljs
@@ -8,9 +8,19 @@
   {:$schema "https://vega.github.io/schema/vega-lite/v5.json"
    :data {:values data}
    :mark {:type "rect" :tooltip true}
-   :encoding {:y {:field "Tumor_Diagnosis" :type :nominal}
-              :x {:field "value" :type :nominal :title false}
-              :facet {:field "dim" :type :nominal :title false :columns 5}
+   :encoding {:y {:field "Tumor_Diagnosis"
+                  :type :nominal
+                  :axis {:labelExpr "replace(datum.label, /_/g, ' ')"}
+                  :title "Tumor Diagnosis"}
+              :x {:field "value"
+                  :type :nominal
+                  :axis {:labelExpr "replace(datum.label, /_/g, ' ')"}
+                  :title false}
+              :facet {:field "dim"
+                      :type :nominal
+                      :title false
+                      :header {:labelExpr "replace(datum.label, /_/g, ' ')"}
+                      :columns 5}
               :color {:field "samples" :type :quantitative
                       :legend {:orient :none :legendX -120 :legendY -75
                                :direction :horizontal
