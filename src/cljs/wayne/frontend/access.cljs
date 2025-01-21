724326fca7d05580b780b57e58dd19cc433322a0
Mike Travers <mt@hyperphor.com>
2025-01-21 15:33:58 -0800

    added more files to data access table

--- src/cljs/wayne/frontend/access.cljs
+++ src/cljs/wayne/frontend/access.cljs
@@ -20,26 +20,106 @@
    {
     :Description "Master Feature Table",
     :File "20240810_master_feature_table_na_removed_metadata.rds",
-    :Size "35M",
-    :Format "rds"
+    :Size "35M"
     },
    {
     :Description "Cell Table (immune)",
     :File "cell_table_immune_thresholded.parquet",
-    :Size "1G",
-    :Format "parquet"
+    :Size "1G"
     },
    {
     :Description "Cell Table (tumor)",
     :File "cell_table_tumor_thresholded.parquet",
-    :Size "1G",
-    :Format "parquet"
+    :Size "1G"
+    }
+   {:File "cell_table_all_merged_thresholded.parquet"
+    :Size "2.0G"
+    :Description "Cell Table (merged)"
+    }
+   {:File "20240702_Stanford_MALDI_annotated.csv"
+    :Size "400K"
+    }
+   {:File "20241112_MALDI_BRUCE_annotated.csv"
+    :Size "809K"
+    }
+   {:File "all_cell_count_tumor_FOV_result.parquet"
+    :Size "7.2K"
+    }
+   {:File "auc_data_survival_status.csv"
+    :Size "310"
+    }
+   {:File "auc_data_who_grade.csv"
+    :Size "309"
+    }
+
+   {:File "gbm_mean_by_stage_table_norm_gly_enz_new.csv"
+    :Size "3.8K"
+    }
+   {:File "glycan_cell_significant_data_fig_5.rds"
+    :Size "658"
+    }
+   {:File "glycan_classes.csv"
+    :Size "6.2K"
+    }
+   {:File "merged_all_top_go_df_fig_5.rds"
+    :Size "2.2K"
+    }
+   {:File "metadata_complete.csv"
+    :Size "316K"
+    }
+   {:File "pca_subset.csv"
+    :Size "982"
+    }
+   {:File "rf_gini_importance_df_survival_status.csv"
+    :Size "764K"
+    }
+   {:File "rf_gini_importance_df_who_grade.csv"
+    :Size "749K"
+    }
+   {:File "tme_gene_signatures.csv"
+    :Size "2.4K"
+    }
+   {:File "top_seven_df.parquet"
+    :Size "37K"
+    }
+   {:File "tumor_count_result.parquet"
+    :Size "7.1K"
     }
    ]
   )
 
 (def cols
   [:Description :File :Size :Format :download])
+
+(defmethod ag/ag-col-def :Format
+  [_ _]
+  {:headerName "Format"
+   :valueGetter (fn [row]
+                  (let [file (.-File (.-data row))]
+                    (second (re-find #"\.(.*)$" file))))})
+
+;;; OK this is dumb, better to store the real number and humanize on output
+(defn dehumanize
+  [hnum]
+  (let [[_ num mag] (re-matches #"([\d\.]+)(\D)?" hnum)
+        num (u/coerce-numeric num)]
+    (case mag
+      nil num
+      "K" (* num 1000)
+      "M" (* num 1000000)
+      "G" (* num 1000000000))))
+
+(defn compare-humanized-nums
+  [a b]
+  (compare (dehumanize a) (dehumanize b)))
+
+(defmethod ag/ag-col-def :Size
+  [_ _]
+  {:headerName "Size"
+   :field :Size
+   :comparator (fn [a b & _]
+                 (compare-humanized-nums a b))})
+
 
 (defmethod ag/ag-col-def :download 
   [col _]
