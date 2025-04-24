55084cbf1276483a0b71c0875ff2c6fe6f70f5e6
Mike Travers <mt@hyperphor.com>
2025-04-23 17:01:30 -0700

    actual hover on nav menu, watta pain

--- src/clj/wayne/templating.clj
+++ src/clj/wayne/templating.clj
@@ -28,9 +28,23 @@
     (zipmap (map #(keyword (second (re-find #"([^/]+)\.html$" %))) comps)
             (map slurp comps))))
 
-(defn expand-page
-  [page-text component-map]
-  (u/expand-template page-text component-map))
+(defn expand-component
+  [component-template component-out params]
+  (-> component-template
+      (u/fsbl str "templates/components/")
+      io/resource
+      slurp
+      (u/expand-template params :allow-missing? true)
+      (u/fsbl spit (str "resources/templates/components/" component-out))))
+
+(defn expand-components
+  []
+  (expand-component "nav.html" "nav-qb.html" {:active-qb "active-qb"})
+  (expand-component "nav.html" "nav-samples.html" {:active-samples "active-samples"})
+  (expand-component "nav.html" "nav-viz.html" {:active-viz "active-viz"})
+  (expand-component "nav.html" "nav-about.html" {:active-about "active-about"})
+  (expand-component "nav.html" "nav-rda.html" {:active-rda "active-rda"})
+  )
 
 ;; Called at Uberjar build time. Won't work at runtime on Heroku
 (defn expand-pages
@@ -41,46 +55,56 @@
     (doseq [[page-key page-content] page-map]
       (log/info "Expanding page" page-key)
       (spit (str "resources/public/pages/" (name page-key) ".html")
-            (expand-page page-content component-map)))))
+            (u/expand-template page-content component-map)))))
+
+
+(defn reinit
+  []
+  (team/generate)
+  (expand-components)
+  (expand-pages))
 
 (defn init
   []
   ;; Expand pages if they aren't present
   (when-not (io/resource "public/pages/about-us.html")
-    (team/generate)
-    (expand-pages)))
+    (reinit)))
 
-(defn reinit
-  []
-  (team/generate)
-  (expand-pages))
 
 ;;; Important: Causes expansion to happen at uberjar build time, for deployment
 
 (init)
 
 
+
+
+
+
+
+
 ;;; <<<>>> ⫷ Component Detection ⫸ <<<>>> ⫷ ⫸ <<<>>> ⫷ ⫸ <<<>>> ⫷ ⫸ <<<>>> ⫷ ⫸ <<<>>> ⫷ ⫸ <<<>>> ⫷ ⫸ <<<>>>
 
-(def files ["about-us.html"
-            "query-builder.html"
-            "raw-data-access.html"
-            "sample-info.html"
-            "visualization.html"])
+(comment
+  (def files ["about-us.html"
+              "query-builder.html"
+              "raw-data-access.html"
+              "sample-info.html"
+              "visualization.html"])
 
-(defn resource-lines
-  [r]
-  (->> r
-       (str "public/pages/")
-       io/resource
-       io/reader
-       line-seq
-       (map str/trim)
-       (filter #(> (count %) 30))))
+  (defn resource-lines
+    [r]
+    (->> r
+         (str "public/pages/")
+         io/resource
+         io/reader
+         line-seq
+         (map str/trim)
+         (filter #(> (count %) 30))))
 
-(defn read-files
-  []
-  (let [lines (mapcat resource-lines files)
-        dupes (filter #(> (second %) 2) (frequencies lines))]
-    dupes))
+  (defn read-files
+    []
+    (let [lines (mapcat resource-lines files)
+          dupes (filter #(> (second %) 2) (frequencies lines))]
+      dupes))
+  )
 
